58e43bd263b7ce687153b161b03791dcaf80c7d1
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-08 01:36:25 +0300

    Update Settings.cs
    add settings

--- CoolParking.BL/Models/Settings.cs
+++ CoolParking.BL/Models/Settings.cs
@@ -2,5 +2,33 @@
 //       Implementation details are up to you, they just have to meet the requirements of the home task.
 namespace CoolParking.BL.Models
 {
+    public static class Settings
+    {
+        public static decimal InitialSum = 0;
+        public static int Capacity = 10;
+        public static int EveryTimePay = 5;
+        public static int EveryTimeWriteToLog = 60;
+        public static double PenaltyRatio = 2.5;
 
+        public static double GetPrice(VehicleType type)
+        {
+            switch (type)
+            {
+                case VehicleType.PassengerCar:
+                    return 2;
+
+                case VehicleType.Bus:
+                    return 3.5;
+
+                case VehicleType.Motorcycle:
+                    return 1;
+
+                case VehicleType.Truck:
+                    return 5;
+
+                default:
+                    return 0;
+            }
+        }
+    }
 }
