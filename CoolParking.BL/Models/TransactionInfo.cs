e5299106fe378443bef8a547354b62fd98eaf0b1
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-08 00:18:31 +0300

    Update TransactionInfo.cs
    Create struct

--- CoolParking.BL/Models/TransactionInfo.cs
+++ CoolParking.BL/Models/TransactionInfo.cs
@@ -1,3 +1,7 @@
 ﻿// TODO: implement struct TransactionInfo.
 //       Necessarily implement the Sum property (decimal) - is used in tests.
 //       Other implementation details are up to you, they just have to meet the requirements of the homework.
+public struct TransactionInfo
+{
+    public decimal Sum { get; set; }
+}
