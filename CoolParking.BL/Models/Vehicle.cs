d5cbda2929a79c0ab59324feee0d2e502ec8bffe
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-08 00:08:59 +0300

    Update Vehicle.cs
    Create class

--- CoolParking.BL/Models/Vehicle.cs
+++ CoolParking.BL/Models/Vehicle.cs
@@ -5,3 +5,9 @@
 //       The Balance should be able to change only in the CoolParking.BL project.
 //       The type of constructor is shown in the tests and the constructor should have a validation, which also is clear from the tests.
 //       Static method GenerateRandomRegistrationPlateNumber should return a randomly generated unique identifier.
+public class Vehicle
+{
+    public string Id { get; set; }
+    public VehicleType VehicleType { get; set; }
+    public decimal Balance { get; set; }
+}
