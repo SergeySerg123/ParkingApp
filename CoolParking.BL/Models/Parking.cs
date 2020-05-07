8a402b3015a52ad258962503b268c7c565934cbc
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-08 00:54:25 +0300

    Update Parking.cs

--- CoolParking.BL/Models/Parking.cs
+++ CoolParking.BL/Models/Parking.cs
@@ -1,3 +1,39 @@
 ﻿// TODO: implement class Parking.
 //       Implementation details are up to you, they just have to meet the requirements 
 //       of the home task and be consistent with other classes and tests.
+
+
+using System.Collections.Generic;
+
+namespace CoolParking.BL.Models
+{
+    public class Parking 
+    {
+        private Parking instance = null;
+
+        public decimal Balance { get; private set; } = 0;
+        private List<Vehicle> Vehicles = new List<Vehicle>();
+
+        private Parking() { }
+
+        public Parking GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Parking();
+            }
+            return instance;
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            Vehicles.Add(vehicle);
+        }
+
+        public void RemoveVehicle(string id)
+        {
+
+        }
+    }
+}
+
