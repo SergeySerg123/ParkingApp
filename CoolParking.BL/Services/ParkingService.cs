572073713f3a30f3ee2ae9adc889698bcf7e9d98
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-08 00:53:57 +0300

    Update ParkingService.cs

--- CoolParking.BL/Services/ParkingService.cs
+++ CoolParking.BL/Services/ParkingService.cs
@@ -4,3 +4,71 @@
 //       Other validation rules and constructor format went from tests.
 //       Other implementation details are up to you, they just have to match the interface requirements
 //       and tests, for example, in ParkingServiceTests you can find the necessary constructor format and validation rules.
+using CoolParking.BL.Interfaces;
+using CoolParking.BL.Models;
+using System.Collections.ObjectModel;
+
+namespace CoolParking.BL.Services
+{
+    public class ParkingService : IParkingService
+    {
+        private Parking Parking;
+
+        public ParkingService()
+        {
+            Parking = Parking.GetInstance();
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            Parking.AddVehicle(vehicle);
+        }
+
+        public void RemoveVehicle(string vehicleId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public decimal GetBalance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetCapacity()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetFreePlaces()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TransactionInfo[] GetLastParkingTransactions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ReadOnlyCollection<Vehicle> GetVehicles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ReadFromLog()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+
+        public void TopUpVehicle(string vehicleId, decimal sum)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
