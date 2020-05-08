0ac19e8a3afbdbcfc16843db788d61c7f04da30d
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-09 01:18:26 +0300

    Create ITransactionService.cs

---
+++ CoolParking.BL/Interfaces/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using CoolParking.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolParking.BL.Interfaces
+{
+    public interface ITransactionService
+    {
+        void CreateTransaction(decimal sum, DateTime dateTime, string vechicleId);
+        TransactionInfo[] GetLastParkingTransactions();
+    }
+}
