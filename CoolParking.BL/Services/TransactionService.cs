7fa8d47bc510fa59b83d2ed91326dcbbc297f3a9
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-09 01:18:03 +0300

    Create TransactionService.cs

---
+++ CoolParking.BL/Services/TransactionService.cs
@@ -0,0 +1,22 @@
+﻿using CoolParking.BL.Interfaces;
+using CoolParking.BL.Models;
+using System;
+
+namespace CoolParking.BL.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly Transactions transactions = new Transactions();
+
+        public void CreateTransaction(decimal sum, DateTime dateTime, string vechicleId)
+        {
+            var transactionInfo = new TransactionInfo(sum, dateTime, vechicleId);
+            transactions.AddParkingTransaction(transactionInfo);
+        }
+
+        public TransactionInfo[] GetLastParkingTransactions()
+        {
+            return transactions.GetLastParkingTransactions();
+        }
+    }
+}
