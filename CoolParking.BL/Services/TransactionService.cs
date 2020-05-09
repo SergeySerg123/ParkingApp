659effcad0369eee0229b83fae8efd47d7a6d335
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-09 10:38:34 +0300

    Update TransactionService.cs

--- CoolParking.BL/Services/TransactionService.cs
+++ CoolParking.BL/Services/TransactionService.cs
@@ -7,16 +7,36 @@ namespace CoolParking.BL.Services
     public class TransactionService : ITransactionService
     {
         private readonly Transactions transactions = new Transactions();
+        private static TransactionService instance = null;
 
-        public void CreateTransaction(decimal sum, DateTime dateTime, string vechicleId)
+        private TransactionService() { }
+
+        public static TransactionService GetInstance()
         {
-            var transactionInfo = new TransactionInfo(sum, dateTime, vechicleId);
+            if (instance == null)
+            {
+                instance = new TransactionService();
+            }
+            return instance;
+        }
+
+        public void CreateTransaction(Parking p, Vehicle vehicle)
+        {
+            var transactionInfo = Withdraw(p, vehicle);
             transactions.AddParkingTransaction(transactionInfo);
         }
 
         public TransactionInfo[] GetLastParkingTransactions()
         {
             return transactions.GetLastParkingTransactions();
+        }
+
+        private TransactionInfo Withdraw(Parking p, Vehicle v)
+        {
+            var priceForHour = Settings.GetPrice(v.VehicleType);
+            decimal actualPrice = p.WithdrawFromVechicle(v, priceForHour);
+            p.TopUpParking(actualPrice);
+            return new TransactionInfo(actualPrice, DateTime.Now, v.Id);
         }
     }
 }
