6d211cafb3b0f8f76a83cd61eb73527ed2916b8b
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-12 23:02:54 +0300

    Create IParkingService.cs

---
+++ CoolParking/CoolParking.WebAPI/Interfaces/IParkingService.cs
@@ -0,0 +1,18 @@
+﻿using CoolParking.WebAPI.Models;
+using System.Collections.ObjectModel;
+
+namespace CoolParking.WebAPI.Interfaces
+{
+    public interface IParkingService
+    {
+        decimal GetBalance();
+        int GetCapacity();
+        int GetFreePlaces();
+        void AddVehicle(Vehicle v);
+        void RemoveVehicle(string vehicleId);
+        void TopUpVehicle(string vehicleId, decimal sum);
+        ReadOnlyCollection<Vehicle> GetVehicles();
+        Vehicle GetVehicle(string vehicleId);
+        TransactionInfo[] GetLastParkingTransactions();
+    }
+}
