a9f4d1b54dc45a6de6ee11a10ac250b7533c2e7a
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-12 23:03:14 +0300

    Create Transactions.cs

---
+++ CoolParking/CoolParking.WebAPI/Models/Transactions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolParking.WebAPI.Models
+{
+    public class Transactions
+    {
+        private readonly List<TransactionInfo> lastTransactions = new List<TransactionInfo>();
+
+        public void AddParkingTransaction(TransactionInfo transactionInfo)
+        {
+            lastTransactions.Add(transactionInfo);
+        }
+        
+        public TransactionInfo[] GetLastParkingTransactions()
+        {
+            return lastTransactions.ToArray();
+        }
+    }
+}
