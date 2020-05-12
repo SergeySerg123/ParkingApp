4e252448ba616be9c3ef57caf51fbd6ad163b633
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-12 23:03:31 +0300

    Create TransactionService.cs

---
+++ CoolParking/CoolParking.WebAPI/Services/TransactionService.cs
@@ -0,0 +1,46 @@
+﻿using CoolParking.WebAPI.Interfaces;
+using CoolParking.WebAPI.Models;
+using System;
+
+namespace CoolParking.WebAPI.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly Transactions transactions = new Transactions();
+        private static TransactionService instance = null;
+
+        private TransactionService() { }
+
+        public static TransactionService GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new TransactionService();
+            }
+            return instance;
+        }
+
+        public void CreateTransaction(Parking p, Vehicle vehicle)
+        {
+            if (vehicle != null)
+            {
+                var transactionInfo = Withdraw(p, vehicle);
+                transactions.AddParkingTransaction(transactionInfo);
+            }
+            
+        }
+
+        public TransactionInfo[] GetLastParkingTransactions()
+        {
+            return transactions.GetLastParkingTransactions();
+        }
+
+        private TransactionInfo Withdraw(Parking p, Vehicle v)
+        {
+            var priceForHour = Settings.GetPrice(v.VehicleType);
+            decimal actualPrice = p.WithdrawFromVechicle(v, priceForHour);
+            p.TopUpParking(actualPrice);
+            return new TransactionInfo(actualPrice, DateTime.Now, v.Id);
+        }
+    }
+}
