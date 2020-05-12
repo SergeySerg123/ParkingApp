b0868ef961c18a71c30df8018d2990af1954e9ef
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-12 13:55:57 +0300

    Create ParkingController.cs

---
+++ CoolParking/CoolParking.WebAPI/Controllers/ParkingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoolParking.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingController : ControllerBase
+    {
+        [Route("balance")]
+        public string Get()
+        {
+            return "Get";
+        }
+    }
+}
