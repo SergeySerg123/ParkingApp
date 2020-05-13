96e85b3b1baace3dd1b9de9278e514d31f96ec67
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-13 08:28:30 +0300

    Create VehicleExtensions.cs

---
+++ CoolParking/CoolParking.WebAPI/Extensions/VehicleExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CoolParking.WebAPI.Interfaces;
+using CoolParking.WebAPI.Models;
+
+namespace CoolParking.WebAPI.Extensions
+{
+    public static class VehicleExtensions
+    {
+        public static VehicleSchema ToVehicleSchema(this Vehicle vehicle)
+            => new VehicleSchema(vehicle);
+    }
+}
