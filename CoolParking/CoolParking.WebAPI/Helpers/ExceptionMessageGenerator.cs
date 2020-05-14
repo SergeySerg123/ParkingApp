67545a12973ee5431eefb4009effb3d3bc13861b
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-14 13:00:16 +0300

    Create ExceptionMessageGenerator.cs

---
+++ CoolParking/CoolParking.WebAPI/Helpers/ExceptionMessageGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CoolParking.WebAPI.Helpers
+{
+    public class ExceptionMessageGenerator
+    {
+        public static string GenereteAgrumentExceptionMessage() => new ArgumentException().Message;
+        public static string GenereteAgrumentNullExceptionMessage() => new ArgumentNullException().Message;
+    }
+}
