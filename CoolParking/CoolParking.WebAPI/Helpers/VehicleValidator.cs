22a61e46cb252d15bac36519d5b6c1495e7fa33b
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-14 13:00:12 +0300

    Create VehicleValidator.cs

---
+++ CoolParking/CoolParking.WebAPI/Helpers/VehicleValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CoolParking.WebAPI.Helpers
+{
+    public static class VehicleValidator
+    {
+        public static bool IsValidVehicleId(string vehicleId)
+        {
+            Regex regex = new Regex(@"\w{2}-\d{4}-\w{2}", RegexOptions.IgnoreCase);
+            return regex.IsMatch(vehicleId);
+        }
+
+        public static bool IsValidTopUpSum(decimal sum) => sum > 0;
+    }
+}
