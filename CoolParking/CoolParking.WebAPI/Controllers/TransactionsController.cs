f196d7453ca9de73d72df77fcdaef834b79d203f
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-14 13:00:24 +0300

    Create TransactionsController.cs

---
+++ CoolParking/CoolParking.WebAPI/Controllers/TransactionsController.cs
@@ -0,0 +1,59 @@
+﻿using CoolParking.WebAPI.Interfaces;
+using static CoolParking.WebAPI.Helpers.VehicleValidator;
+using static CoolParking.WebAPI.Helpers.ExceptionMessageGenerator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CoolParking.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IParkingService _parkingService;
+
+        public TransactionsController(IParkingService parkingService)
+        {
+            _parkingService = parkingService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetAllTransactions()
+        {
+            var transactions = _parkingService.GetLastParkingTransactions();
+            return Ok(transactions);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetLastTransactions()
+        {
+            //NOT IMPLEMENT
+            var transactions = _parkingService.GetLastParkingTransactions();
+            return Ok(transactions);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult TopUpVehicle(string id, decimal sum)
+        {
+            bool isValidId = IsValidVehicleId(id);
+            if(isValidId)
+            {
+                var vehicle = _parkingService.TopUpVehicle(id, sum);
+                if (vehicle != null)
+                {
+                    return Ok();
+                }
+                return NotFound(GenereteAgrumentNullExceptionMessage());
+            }
+            return BadRequest(GenereteAgrumentExceptionMessage());
+        }
+
+    }
+}
