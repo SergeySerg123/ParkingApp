7ba18885c7fd8b62af9cb12d2b75a0c278f0eaf7
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-14 12:59:55 +0300

    Create ITransactionsService.cs

---
+++ CoolParking/CoolParking.WebAPI/Interfaces/ITransactionsService.cs
@@ -0,0 +1,14 @@
+﻿using CoolParking.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolParking.WebAPI.Interfaces
+{
+    public interface ITransactionsService
+    {
+        void CreateTransaction(Parking p, Vehicle vehicle);
+        TransactionInfo[] GetLastParkingTransactions();
+    }
+}
